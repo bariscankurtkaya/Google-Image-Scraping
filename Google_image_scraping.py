c57a42d47e2dd357ba9f35e77a3bbff8398b0ffa
Barışcan Kurtkaya <bariscankurtkaya@gmail.com>
2020-09-30 17:22:08 +0300

    Update Google_image_scraping.py

--- Google_image_scraping.py
+++ Google_image_scraping.py
@@ -74,6 +74,7 @@ class SearchPatent():
         header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
         soup = self.get_soup(url,header)
         b=0
+        
         #Scraping HTML and get Image
         for a in soup.find_all("a",{"class":"wXeWr"}):
             try:
